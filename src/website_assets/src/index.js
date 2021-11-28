397659a9ea74c78a04c27ade6d437135b1e7bc45
Michael McGreal <mcgrealife@gmail.com>
2021-11-27 21:04:31 -0500

    Initial commit.

---
+++ src/website_assets/src/index.js
@@ -0,0 +1,19 @@
+import { website } from "../../declarations/website";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await website.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
